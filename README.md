eebba7dbf1aea7842fb37f1bffe329b8f23976be
prachipal <57104486+prachipal1998@users.noreply.github.com>
2023-12-01 17:33:20 -0500

    empty

--- README.md
+++
@@ -1,2 +0,0 @@
-# webprogproject
-# Chintan
--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = { 
-    url :   process.env.DATABASE_URL || "mongodb+srv://prachipal205:prachi@cluster0.larhym1.mongodb.net/sample_restaurants"
-};
